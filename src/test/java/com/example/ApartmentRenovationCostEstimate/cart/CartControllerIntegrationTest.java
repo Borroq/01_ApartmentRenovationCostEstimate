ffa895c4cca05369aa8f5de6358592b687c8d348
Damian <skrzypek.dsrw@gmail.com>
2025-03-11 19:22:52 +0100

    Refactor

--- src/test/java/com/example/ApartmentRenovationCostEstimate/cart/CartControllerIntegrationTest.java
+++ src/test/java/com/example/ApartmentRenovationCostEstimate/cart/CartControllerIntegrationTest.java
@@ -1,7 +1,7 @@
 package com.example.ApartmentRenovationCostEstimate.cart;
 
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
